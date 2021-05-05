a9bcc1d4d46ec9bd5d9a39bde8b5d3b23d7298a3
Edward Boland <mr.eboland@gmail.com>
2021-05-05 15:30:43 -0400

    practice q for tuples and arrays

--- src/main.rs
+++ src/main.rs
@@ -58,7 +58,7 @@ fn main() {
 
     // Vectors also support indexing
     let mut x = vec![1, 2, 3];
-    let three = x[2];
+    let _three = x[2];
     x[1] = x[1] + 5;
     println!("{}", x[1]);
 
@@ -127,4 +127,36 @@ fn main() {
     assert_eq!(book_reviews.contains_key(sherlock), true);
     book_reviews.remove(sherlock);
     assert_eq!(book_reviews.contains_key(sherlock), false);
+
+    indexing_tuple(); // no errors, works
+    indexing_array(); // no errors, works
+}
+
+
+fn indexing_tuple() {
+    let numbers = (1, 2, 3);
+    // TODO - Correct the syntax
+    // let second = todo!("Replace with the tuple indexing syntax");
+    // To access a tuple we use dot notation
+    let second = numbers.1;
+
+    assert_eq!(
+        2, second,
+        "This is not the 2nd number in the tuple: {}",
+        second
+    )
+}
+
+fn indexing_array() {
+    let characters = ['a', 'b', 'c', 'd', 'e'];
+    // TODO - Correct the syntax
+    // let letter_d = todo!("Replace with the array indexing syntax");
+    // Like all arrays, we access the value using bracket notation.
+    let letter_d = characters[3];
+
+    assert_eq!(
+        'd', letter_d,
+        "This is not the character for the letter d: {}",
+        letter_d
+    )
 }
